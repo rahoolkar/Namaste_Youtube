1d93e558e6994d8825b926a611a3a0cbdebbc357
rahoolkar <batman30jan2002@gmail.com>
2026-05-25 13:33:00 +0530

    buttons fixed

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,4 +1,5 @@
 import Button from "./Button";
+import { useRef } from "react";
 
 function ButtonList() {
   const buttonList = [
@@ -26,19 +27,48 @@ function ButtonList() {
     "Notion",
     "Gym",
   ];
+
+  const containerRef = useRef(null);
+
+  function handleScroll(direction) {
+    const scrollAmount = 300;
+
+    if (direction == "left") {
+      containerRef.current.scrollBy({
+        left: -scrollAmount,
+        behavior: "smooth",
+      });
+    } else {
+      containerRef.current.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    }
+  }
+
   return (
     <div className="relative">
-      <button className="absolute left-0 top-1/4 bg-black text-amber-50 p-2 rounded-full z-10 px-3
-        py-2 cursor-pointer">
+      <button
+        onClick={() => {
+          handleScroll("left");
+        }}
+        className="absolute left-0 top-1/4 bg-black text-amber-50 p-2 rounded-full z-10 px-3
+        py-2 cursor-pointer"
+      >
         ←
       </button>
-      <div className="flex overflow-x-scroll scroll-smooth scrollbar-none pt-4 pb-4">
+      <div
+        className="flex overflow-x-scroll scroll-smooth scrollbar-none pt-4 pb-4"
+        ref={containerRef}
+      >
         {buttonList.map((button, index) => {
           return <Button key={index} value={button}></Button>;
         })}
       </div>
-      <button className="absolute right-0 top-1/4 bg-black text-amber-50 p-2 rounded-full z-90 px-3
-        py-2 cursor-pointer">
+      <button
+        onClick={() => {
+          handleScroll("right");
+        }}
+        className="absolute right-0 top-1/4 bg-black text-amber-50 p-2 rounded-full z-10 px-3
+        py-2 cursor-pointer"
+      >
         →
       </button>
     </div>
