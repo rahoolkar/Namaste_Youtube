b32e5c88bd1a46719d5c23e365f090cf949c39e9
rahoolkar <batman30jan2002@gmail.com>
2026-06-25 02:28:42 +0530

    count function added

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -20,6 +20,7 @@ function VideoDetails() {
   const [channelDetails, setChannelDetails] = useState(null);
   const [isLiked, setIsLiked] = useState(false);
   const [isDisLiked, setIsDisLiked] = useState(false);
+  const [shouldExpand, setShouldExpand] = useState(false);
 
   useEffect(() => {
     dispatch(collapseLeftCatergory());
@@ -42,7 +43,6 @@ function VideoDetails() {
       `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,contentDetails,status,topicDetails&id=${vid}&key=${YOUTUBE_API_KEY}`,
     );
     const json = await response.json();
-    console.log(json.items[0]);
     setVideoDetails(json.items[0]);
   }
 
@@ -51,7 +51,7 @@ function VideoDetails() {
       YOUTUBE_GET_CHANNEL_DETAILS + channelId + "&key=" + YOUTUBE_API_KEY,
     );
     const json = await response.json();
-    setChannelDetails(json.items[0].snippet);
+    setChannelDetails(json.items[0]);
     console.log(json.items[0]);
   }
 
@@ -69,7 +69,16 @@ function VideoDetails() {
     setIsDisLiked(!isDisLiked);
   }
 
+  console.log(channelDetails);
+
   const { title, channelId, channelTitle } = videoDetails.snippet;
+
+  function formatCount(num) {
+    return Intl.NumberFormat("en", {
+      notation: "compact",
+      maximumFractionDigits: 1,
+    }).format(num);
+  }
 
   return (
     <div className="w-full lg:w-[75vw] p-2">
@@ -86,7 +95,7 @@ function VideoDetails() {
         <h3 className="text-lg font-bold text-black line-clamp-1">{title}</h3>
         <div className="flex justify-between">
           <div className="flex items-center">
-            <div className="w-9 h-9 rounded-full">
+            <div className="w-9 h-9 rounded-full overflow-hidden">
               <img
                 src={
                   channelDetails?.snippet?.thumbnails?.high?.url ||
@@ -96,46 +105,48 @@ function VideoDetails() {
               />
             </div>
 
-            <div className="flex flex-col justify-center ml-2">
+            <div className="hidden md:flex flex-col justify-center ml-2">
               <h3 className="font-bold text-sm">{channelTitle}</h3>
               <p className="text-gray-500 text-sm">
-                {channelDetails?.statistics?.subscriberCount || 999} subscribers
+                {formatCount(channelDetails?.statistics?.subscriberCount) || 0}
+                subscribers
               </p>
             </div>
 
-            <button className="bg-black text-white hover:bg-red-600 ml-3 px-4 py-2 rounded-3xl h-9 text-center flex items-center cursor-pointer">
+            <button className="bg-black text-white hover:bg-red-600 ml-1 md:ml-3 px-4 py-2 rounded-3xl h-9 text-center flex items-center cursor-pointer">
               Subscribe
             </button>
           </div>
 
           <div className="flex items-center">
-            <div className="flex items-center p-2">
+            <div className="flex items-center p-1 md:p-2">
               <div
-                className="text-2xl p-2 hover:bg-gray-100 rounded-l-full flex items-center cursor-pointer"
+                className="text-2xl p-1 md:p-2 hover:bg-gray-100 rounded-l-full flex items-center cursor-pointer"
                 onClick={handleLike}
               >
                 {isLiked ? <AiFillLike /> : <AiOutlineLike />}{" "}
-                <span className="text-sm font-semibold ml-1">
-                  {videoDetails?.statistics?.likeCount / 1000} K
+                <span className="text-sm font-semibold ml-1 hidden md:inline-block">
+                  {formatCount(videoDetails?.statistics?.likeCount) || 0}
                 </span>
               </div>
 
-              <div className="border border-l border-gray-400 h-full"></div>
+              <div className="border border-l border-gray-400 h-full hidden md:inline"></div>
               <div
-                className="text-2xl p-2 hover:bg-gray-100 rounded-r-full cursor-pointer"
+                className="text-2xl p-1 md:p-2 hover:bg-gray-100 rounded-r-full cursor-pointer"
                 onClick={handleDisLike}
               >
                 {isDisLiked ? <AiFillDislike /> : <AiOutlineDislike />}
               </div>
             </div>
 
-            <div className="flex items-center hover:bg-gray-100 rounded-4xl py-2 px-3 cursor-pointer">
-              <RiShareForwardLine size={20} />{" "}
-              <span className="font-semibold ml-2">Share</span>
+            <div className="text-xl items-center hover:bg-gray-100 rounded-4xl p-1 md:py-2 md:px-3 cursor-pointer hidden md:flex">
+              <RiShareForwardLine size={20} className="text-xl" />
+              <span className="ml-2 text-lg hidden md:inline">Share</span>
             </div>
           </div>
         </div>
-        <div></div>
+
+        <div>{}</div>
       </div>
     </div>
   );
